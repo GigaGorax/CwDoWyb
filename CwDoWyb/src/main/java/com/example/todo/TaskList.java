320b2bb1f49304906ab03125fa35a2421c741935
GigaGorax <mateuszklysiaksm@gmail.com>
2023-06-24 11:19:23 +0200

    dodano trzeci branch

--- CwDoWyb/src/main/java/com/example/todo/TaskList.java
+++ CwDoWyb/src/main/java/com/example/todo/TaskList.java
@@ -1,2 +1,24 @@
-package com.example.todo;public class TaskList {
+package com.example.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return tasks;
+    }
 }
